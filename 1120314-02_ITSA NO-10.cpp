6a664c256a387a3c7b90ed6637e2e70f3a9ee8b4
士釗 馮 <4b1g0002@gmail.com>
2023-03-14 12:12:35 +0800

    加入專案檔案。

---
+++ 1120314-02_ITSA NO-10.cpp
@@ -0,0 +1,27 @@
+﻿// 1120314-02_ITSA NO-10.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+// 題目10. 輾轉相除法
+
+#include<iostream>
+using namespace std;
+
+int euc(int a, int b);//輾轉相除法遞迴函式宣告 
+int main()
+{
+	int input = 0, input2 = 0, c = 0;
+
+	cin >> input >> input2;
+	if (input < input2)//確認大小順序 
+	{
+		c = input;
+		input = input2;
+		input2 = c;
+	}
+	cout << euc(input, input2) << endl;
+	return 0;
+}
+int euc(int a, int b)
+{
+	int f = a % b;
+	if (f == 0) return b;
+	else return euc(b, f);
+}
